f078416904e106aa9184bbbe39e165c344d3ed3e
Odin <odinuv@gmail.com>
2016-01-22 17:57:43 +0100

    fix: typo feat: added multiple repositories for more reliable package loading fix: remove R application which is not necessary

--- init.R
+++ init.R
@@ -1,5 +1,12 @@
+repoList <- c('https://cran.rstudio.com/', 'http://cran.us.r-project.org', 'https://cran.cnr.berkeley.edu/', 'https://cran.mtu.edu/', 'https://cran.fhcrc.org/'),
+
 # install really required packages
-install.packages(c('curl', 'DBI', 'devtools', 'git2r', 'jsonlite', 'rJava', 'RJDBC', 'roxygen2', 'rversions', 'rstudioapi'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
+install.packages(
+	c('curl', 'DBI', 'devtools', 'git2r', 'jsonlite', 'rJava', 'RJDBC', 'roxygen2', 'rversions', 'rstudioapi'), 
+	repos = repoList,
+	dependencies = c("Depends", "Imports", "LinkingTo"), 
+	INSTALL_opts = c("--no-html")
+)
 
 # install some commonly used packages
 install.packages(
@@ -17,19 +24,13 @@ install.packages(
 		'quantreg',
 		'R6', 'Rcpp',
 		'stringi', 'stringr', 'stats',
-		'timeDate'
+		'timeDate',
     	'utils',
     	'whisker',
     	'xml2',
     	'zoo'
 	), 
-	repos = 'http://cran.us.r-project.org', 
+	repos = repoList,
 	dependencies = c("Depends", "Imports", "LinkingTo"), 
 	INSTALL_opts = c("--no-html")
 )
-
-library('devtools')
-
-# install the R application
-install_github('keboola/r-application', ref = "master")
-install_github('keboola/r-docker-application', ref = "master")
